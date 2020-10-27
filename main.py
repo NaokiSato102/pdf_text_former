dafb4d038a069bd076e9df609238e18ff1deee6e
NaokiSato102 <s17c1066nq@s.chibakoudai.jp>
2020-10-28 00:31:42 +0900

    テキストエディタでなく自前のGUIを実装

--- main.py
+++ main.py
@@ -2,36 +2,87 @@
 # -*- coding: utf-8 -*-
 
 
+'''
+論文等のPDFからの文字のパーサ
+'''
 
-'''f = open('input.txt','r',encoding='UTF-8')
-text = f.read()
-f.close()
-formated_text = text
-formated_text = formated_text.replace('\n',' ')
-formated_text = formated_text.replace('.','.\n')
+# Variable name "ほげ" doesn't conform to snake_case naming style
+# pylint: disable=C0103
 
-f = open('output.txt','w',encoding='UTF-8')
-f.write(formated_text)
-f.close()'''
 
-import time
+import sys
 import pyperclip as clip
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
 
 
 
-text = ''
-pre_text = ''
-while 1:
-	f = open('input.txt','r',encoding='UTF-8')
-	text = f.read()
-	f.close()
 
-	if pre_text != text:
-		pre_text = text
-		text = text.replace('\n',' ')
-		text = text.replace('.','.\n')
 
-		print('内容を整形しました！')
+def text_convert(text,mode=0):
+	"""
+		テキストの不要な改行を除いた後、句読点で再度改行を付与する
+	"""
+	text = text.replace('\n','')
+
+	if mode == 0:
+		text = text.replace('．','．\n')
+
+
+	return text
+
+class PTF_UI(QtWidgets.QMainWindow):
+	"""
+		hoge
+	"""
+	def __init__(self, parent=None):
+
+		QtWidgets.QMainWindow.__init__(self)
+		UI_class = uic.loadUiType("UI.ui", self)[0]
+
+		self.ui = UI_class()
+		self.ui.setupUi(self)
+
+		self.ui.Textbox_red.setPlainText(clip.paste())
+
+		self.ui.Botton_read_cb.clicked.connect(self.Botton_read_cb_clicked)
+		self.ui.Button_run.clicked.connect(self.Button_run_clicked)
+		self.ui.Button_write.clicked.connect(self.Button_write_clicked)
+
+
+	@QtCore.pyqtSlot()
+	def Button_run_clicked(self):
+		"""
+			変換前の文字列を変換し、変換後窓とクリップボードに書出し
+		"""
+		text = self.ui.Textbox_red.toPlainText()
+		print(f"[debug]変換前[{text}]")
+
+		text = text_convert(text)
+		print(f"[debug]変換後[{text}]")
+
+		self.ui.Textbox_send.setPlainText(text)
 		clip.copy(text)
 
-	time.sleep(1)
+
+	@QtCore.pyqtSlot()
+	def Botton_read_cb_clicked(self):
+		"""
+			クリップボードから読込み
+		"""
+		self.ui.Textbox_red.setPlainText(clip.paste())
+
+	@QtCore.pyqtSlot()
+	def Button_write_clicked(self):
+		"""
+			クリップボードへ書出し
+		"""
+		text = self.ui.Textbox_send.toPlainText()
+		clip.copy(text)
+
+if __name__ == '__main__':
+	#main()
+	app = QtWidgets.QApplication(sys.argv)
+	window = PTF_UI()
+	window.show()
+
+	sys.exit(app.exec_() )
